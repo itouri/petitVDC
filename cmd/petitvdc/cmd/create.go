d34a73cfe1b6c8d91054f3671fb9da6ea528db65
itouri <k-akiyama@axsh.net>
2018-02-17 15:19:30 +0900

    Made simple create command

--- cmd/petitvdc/cmd/create.go
+++ cmd/petitvdc/cmd/create.go
@@ -1,45 +1,48 @@
 package cmd
 
 import (
-	"../../../api"
-	"../internal/util"
+	"fmt"
+
 	"github.com/spf13/cobra"
-	"google.golang.org/grpc"
 )
 
 var createCmd = &cobra.Command{
-	Use: "create [ResourceTemplate.json]",
+	Use:   "create [ResourceTemplate.json]",
+	Short: "Register an instance from template",
+	Long:  `Register an instance from template.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return pflag.ErrHelp // ???
-		}
-		req := prepareCreateAPICall(args[0], args[1:], cmd.Flags())
-
+		// if len(args) < 1 {
+		// 	return pflag.ErrHelp // ???
+		// }
+		// req := prepareCreateAPICall(args[0], args[1:], cmd.Flags())
+		// call := func(conn *grpc.ClientConn) error {
+		// 	c := api.NewInstanceClient(conn)
+		// 	reply, err := c.Create(context.Background()), req) // req はクロージャ渡し
+		// 	if err != nil {
+		// 		log.WithError(err).Fatal("Disconnected abnormaly")
+		// 		return err
+		// 	}
+		// 	fmt.Println(reply.GetInstanceId())
+		// 	return err
+		// }
+		// return util.RemoteCall(call)
+		fmt.Println("OK")
+		return nil
 	},
 }
 
-func prepareCreateAPICall(templateSlug string, args []string, flags *pflag.FlagSet) *api.CreateRequest, error {
-	// TODO URLで取ってるjsonとかに対応するのは後
-	// パスを基にtemplate.jsonを見つける
-	tmpl, err := util.FetchTemplate(templateSlug)
-	if err != nil {
-		nil, err
-	}
+// func prepareCreateAPICall(templateSlug string, args []string, flags *pflag.FlagSet) *api.CreateRequest, error {
+// 	// TODO URLで取ってるjsonとかに対応するのは後
+// 	// パスを基にtemplate.jsonを見つける
+// 	tmpl, err := util.FetchTemplate(templateSlug)
+// 	if err != nil {
+// 		nil, err
+// 	}
 
-	// TODO オプションに追加されたパラメータをjsonにくっつける
+// 	// TODO オプションに追加されたパラメータをjsonにくっつける
 
-	req := &api.CreateRequest {
-		InstanceTemplate: tmpl,
-	}
-	call := func(conn *grpc.ClientConn) error {
-		c := api.NewInstanceClient(conn)
-		reply, err := c.Create(context.Background()), req)
-		if err != nil {
-			log.WithError(err).Fatal("Disconnected abnormaly")
-			return err
-		}
-		fmt.Println(reply.GetInstanceId())
-		return err
-	}
-	return util.RemoteCall(call)
-}
+// 	req := &api.CreateRequest {
+// 		InstanceTemplate: tmpl,
+// 	}
+// 	return req
+// }
--- cmd/petitvdc/cmd/root.go
+++ cmd/petitvdc/cmd/root.go
@@ -7,15 +7,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func init() {
+	cobra.OnInitialize()
+}
+
 var RootCmd = &cobra.Command{
 	Use:   "petitvdc",
 	Short: "",
 	Long:  ``,
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 func Execute() {
 	RootCmd.AddCommand(createCmd)
-	if err := RootCmd.Execute; err != nil {
+	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1) // ??? -1?
 	}
